e186f449d7f4f88d34631902e19e4f2c97a72eea
prathmesh873 <prathmeshc487@gmail.com>
2026-06-02 21:52:48 +0530

    Update Landing page with Prathmesh Chavan info

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,20 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              AKASH
+              PRATHMESH
               <br />
-              <span>MALHOTRA</span>
+              <span>CHAVAN</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>Co-Founder &</h3>
+            <h3>CEO &</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Tech</div>
-              <div className="landing-h2-2">Business</div>
+              <div className="landing-h2-1">Performance</div>
+              <div className="landing-h2-2">Marketer</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Business</div>
-              <div className="landing-h2-info-1">Tech</div>
+              <div className="landing-h2-info">AI-Enabled</div>
+              <div className="landing-h2-info-1">Builder</div>
             </h2>
           </div>
         </div>
