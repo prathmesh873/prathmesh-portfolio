32da9fdcfe6cb4c016edd77c6fa25944bc10f503
prathmesh873 <prathmeshc487@gmail.com>
2026-06-06 17:36:26 +0530

    Update Navbar with Prathmesh Chavan initials (PC)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,6 @@ const Navbar = () => {
       autoResize: true,
       ignoreMobileResize: true,
     });
-
     smoother.scrollTop(0);
     smoother.paused(true);
 
@@ -35,24 +34,17 @@ const Navbar = () => {
         }
       });
     });
+
     window.addEventListener("resize", () => {
       ScrollSmoother.refresh(true);
     });
   }, []);
+
   return (
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
-        </a>
-        <a
-          href="https://www.linkedin.com/in/akashrmalhotra/"
-          className="navbar-connect"
-          data-cursor="disable"
-          target="_blank"
-          rel="noreferrer"
-        >
-          linkedin.com/in/akashrmalhotra
+          PC
         </a>
         <ul>
           <li>
@@ -72,7 +64,6 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-
       <div className="landing-circle1"></div>
       <div className="landing-circle2"></div>
       <div className="nav-fade"></div>
