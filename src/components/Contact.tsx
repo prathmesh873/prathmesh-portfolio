0bf5eaba7d03f6cd3f868f16de1330a2ac246775
prathmesh873 <prathmeshc487@gmail.com>
2026-06-06 17:38:57 +0530

    Update contact information and links in Contact.tsx
    Update Contact.tsx with Prathmesh Chavan details

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,27 +11,23 @@ const Contact = () => {
             <h4>Connect</h4>
             <p>
               <a
-                href="https://www.linkedin.com/in/akashrmalhotra/"
+                href="https://www.linkedin.com/in/prathmesh873/"
                 target="_blank"
                 rel="noreferrer"
                 data-cursor="disable"
               >
-                LinkedIn — akashrmalhotra
+                LinkedIn — prathmesh873
               </a>
             </p>
             <h4>Education</h4>
             <p>
-              PGPWE (MBA), Indian Institute of Management, Lucknow — 2021–2023
-            </p>
-            <p>
-              B.Tech Computer Science, Manav Rachna Educational Institutions —
-              2009–2013
+              BBA, Symbiosis International University, Pune
             </p>
           </div>
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/akashrmalhotra"
+              href="https://github.com/prathmesh873"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -40,7 +36,7 @@ const Contact = () => {
               GitHub <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com/in/akashrmalhotra/"
+              href="https://www.linkedin.com/in/prathmesh873/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -49,27 +45,19 @@ const Contact = () => {
               LinkedIn <MdArrowOutward />
             </a>
             <a
-              href="https://www.youtube.com/@Leftbraincoder"
+              href="mailto:prathmeshc487@gmail.com"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
               className="contact-social"
             >
-              YouTube <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.instagram.com/leftbraincoder/"
-              target="_blank"
-              rel="noreferrer"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Instagram <MdArrowOutward />
+              Email <MdArrowOutward />
             </a>
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Akash Malhotra</span>
+              Designed and Developed <br />
+              by <span>Prathmesh Chavan</span>
             </h2>
             <h5>
               <MdCopyright /> 2026
